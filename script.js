fc264bb8cf2a884f313a8e8bd41db43afadcc291
Pavel Bekk <begonknees@mail.ru>
2017-05-08 23:04:20 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ $(document).ready(function(){
 	var ppl = 100; var mon = 1000;
 	var randomPPL, randomTERS, randomMON;
 	var terra = 100;
+	var townsfp = ["Асгорд", "Пантелеода", "Украина", "Летзария", "Гондор", "Молдавия", "Германия", "Пруссия"];
+	var gosudarstvoname = "undefined";
+
 
 	//игровые элементы
 	$("#game").hide();
@@ -12,12 +15,34 @@ $(document).ready(function(){
 	$("#mon").hide();
 	$("#event").hide();
 	$("fire").hide();
-	$("#results").hide();
-	
+	$("#start").hide();
+	$("#results").hide();	
+	$("#days").hide();
+	$("#btn").hide();
+
+	//название
+	$("#gosnameok").click(function(event) {
+		gosudarstvoname = $("#gosname").val();
+		if(gosudarstvoname == "") {
+			index = Math.floor(Math.random() * (7 - 0)) - 0;
+			$("#start").text("Государство: " + townsfp[index]);
+			$("#start").show();
+			$("#btn").show();
+			$("#days").show();
+			$("#chusnaem").hide();
+		}
+		else { $("#start").text("Государство: " + gosudarstvoname); 
+			   $("#start").show();
+			   $("#btn").show();
+			   $("#days").show();
+		       $("#chusnaem").hide();
+		} 
+	});
+
 	$("#btn").click(function(event){
 
 		$("#event").hide();
-		$("fire").hide();
+		$("#fire").hide();
 
 		//каунтер дней
 		i++;
@@ -35,27 +60,26 @@ $(document).ready(function(){
 		if (i < 31){
 			randomMON = Math.floor(Math.random() * (100 - 10)) + 10;
 			randomPPL = Math.floor(Math.random() * (50 - 5)) + 5;
-			randomTERS = Math.floor(Math.random() * (3 - 0)) + 0;
 
 			ppl = ppl + randomPPL;
 			mon = mon + randomMON;
 
 			$("#ppl").text("Население: " + ppl + " человек");
-			$("#mon").text("Казна: " + mon + " рублей");
+			$("#mon").text("Казна: " + mon + " монет");
 			$("#ters").text("Территория: " + terra + "/100 %")
 
 			//евенты
 			randomer = Math.floor(Math.random() * (100 - 1)) + 1;
 			if(randomer < 10){
 				$("#event").show();
-				$("fire").show();
+				$("#fire").show();
 
 				ppl = ppl - 150;
 				mon = mon - 450;
 				terra = terra - 10;
 
 				$("#ppl").text("Население: " + ppl + " человек");
-				$("#mon").text("Казна: " + mon + " рублей");
+				$("#mon").text("Казна: " + mon + " монет");
 				$("#ters").text("Территория: " + terra + "/100 %")
 			}
 		}
@@ -93,6 +117,6 @@ $(document).ready(function(){
 			$("#mon").show();
 			$("#days").text("Конец игры. Страна обанкротилась.");
 		}
-	})
+	});
 
 });
