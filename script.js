7a1d5ea8482d2ce9ae9b8c967b3323e7994fba88
Pavel Bekk <begonknees@mail.ru>
2017-05-08 18:05:57 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,40 @@
 $(document).ready(function(){
 	var i = 0;
-	$("#ripnikita").hide();
+	var ppl = 100; var mon = 1000;
+	var randomPPL, randomTERS, randomMON;
 
+	//игровые элементы
+	$("#game").hide();
+	$("#ppl").hide();
+	$("#ters").hide();
+	$("mon").hide();
+	
 	$("#btn").click(function(event){
-		i++;
-		$("#obama").show();
 
+		//каунтер дней
+		i++;
+		$("#days").text("День: " + i);
+
+		//стандартные показатели
 		if (i == 1){
-			$("#ripnikita").show();
+			$("#game").show();
+			$("#ppl").show();
+			$("#ters").show();
+			$("mon").show();
+		}
+
+
+		//первые 20 дней
+		if (i > 1){
+			randomMON = Math.floor(Math.random() * (100 - 10)) + 10;
+			randomPPL = Math.floor(Math.random() * (50 - 5)) + 5;
+
+			ppl = ppl + randomPPL;
+			mon = mon + randomMON;
+
+			$("#ppl").text("Население: " + ppl);
+			$("#mon").text("Казна: " + mon);
 		}
 	})
+
 });
